0c837fa57091e8a3aea1cd40bc2f97b739c106b4
sebastian-sonne <sebastian@familie-sonne.com>
2025-01-15 21:22:57 +0100

    bug fix

--- src/state/thunks/assingStudent.thunk.ts
+++ src/state/thunks/assingStudent.thunk.ts
@@ -1,10 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { assignStudent as gridAssign, Coordinates } from "../slices/gridSlice";
 import { setIsAssigned } from "../slices/studentSlice";
+import { RootState } from "../store";
 
 export const assignStudent = createAsyncThunk(
     'students/assignStudent',
-    async ({ id, coords }: { id: string, coords: Coordinates }, { dispatch }) => {
+    async ({ id, coords }: { id: string, coords: Coordinates }, { getState, dispatch }) => {
+        const state = getState() as RootState;
+        const prevStudentId = state.grid.deskSetup[coords.row][coords.col].studentId;
+        if (prevStudentId) {
+            dispatch(setIsAssigned({ id: prevStudentId, val: false }))
+        }
+
         dispatch(gridAssign({ ...coords, id }));
         dispatch(setIsAssigned({ id, val: true }))
     }
