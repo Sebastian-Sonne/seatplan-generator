d48db3b396507173acc1acc6c7e5640ed08884b8
sebastian-sonne <sebastian@familie-sonne.com>
2025-01-25 18:40:33 +0100

    footer update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
         <div className='relative bottom-2 flex justify-center align-middle w-full h-6 my-2'>
             <p className="text-text-muted-extra text-sm font-semibold">&copy; {new Date().getFullYear()}
                 <a href="https://github.com/sebastian-sonne"
-                    className="hover:text-text-muted transition-colors" target="_blank"> Sebastian Sonne</a>
+                    className="hover:text-text-muted transition-colors" target="_blank"> Seatplan.xyz</a>
             </p>
         </div>
     );
